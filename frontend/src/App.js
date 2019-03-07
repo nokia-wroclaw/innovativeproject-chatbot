94dfe15fc32fbe1c250c4bd6a6b96ecd35cc8b1c
Kamil <kamil4525@gmail.com>
2019-03-07 16:56:18 +0100

    Setup frontend (ReactJS)
    Imported dependencies: bootstrap, redux, react-redux, react-router-dom, axios, classnames

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+class App extends Component {
+    render() {
+        return(
+            <div>
+            <h1 className="alert alert-warning">Hello React!</h1>
+            </div>
+        )
+    }
+}
+
+export default App
