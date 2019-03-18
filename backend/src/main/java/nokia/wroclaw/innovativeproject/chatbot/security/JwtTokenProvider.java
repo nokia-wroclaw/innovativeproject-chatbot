ca690905436270748b93d1fc315daeae6ff753af
Kamil <kamil4525@gmail.com>
2019-03-18 11:33:44 +0100

    allow to access data after logging in

--- backend/src/main/java/nokia/wroclaw/innovativeproject/chatbot/security/JwtTokenProvider.java
+++ backend/src/main/java/nokia/wroclaw/innovativeproject/chatbot/security/JwtTokenProvider.java
@@ -1,7 +1,6 @@
 package nokia.wroclaw.innovativeproject.chatbot.security;
 
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 import nokia.wroclaw.innovativeproject.chatbot.domain.User;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
@@ -40,7 +39,29 @@ public class JwtTokenProvider {
     }
 
     // validate the token
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(SECRET).parseClaimsJws(token);
+            return true;
+        } catch (SignatureException ex) {
+            System.out.println("Invalid JWT Signature");
+        } catch (MalformedJwtException ex) {
+            System.out.println("Invalid JWT Token");
+        } catch (ExpiredJwtException ex) {
+            System.out.println("Expired JWT Token");
+        } catch (UnsupportedJwtException ex) {
+            System.out.println("Unsupported JWT Token");
+        } catch (IllegalArgumentException ex) {
+            System.out.println("JWT claims string is empty");
+        }
+        return false;
+    }
 
     // get user Id from token
+    public Long getUserIdFromJWT(String token) {
+        Claims claims = Jwts.parser().setSigningKey(SECRET).parseClaimsJws(token).getBody();
+        String id = (String) claims.get("id");
+        return Long.parseLong(id);
+    }
 
 }
--- backend/src/main/java/nokia/wroclaw/innovativeproject/chatbot/security/SecurityConfig.java
+++ backend/src/main/java/nokia/wroclaw/innovativeproject/chatbot/security/SecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static nokia.wroclaw.innovativeproject.chatbot.security.SecurityConstants.H2_URL;
 import static nokia.wroclaw.innovativeproject.chatbot.security.SecurityConstants.SIGN_UP_URLS;
@@ -31,6 +32,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private CustomUserDetailsService customUserDetailsService;
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter() {
+        return new JwtAuthenticationFilter();
+    }
 
     @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
@@ -70,5 +76,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(SIGN_UP_URLS).permitAll()
                 .antMatchers(H2_URL).permitAll()
                 .anyRequest().authenticated();
+
+        http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 }
