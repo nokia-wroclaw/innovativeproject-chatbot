03d15afe6255c23f55588ae1ce090ab6280c2473
Patryk Antosz <papoj@wp.pl>
2019-04-09 22:52:56 +0200

    Some code to review

--- backend/src/main/java/nokia/wroclaw/innovativeproject/chatbot/ChatbotApplication.java
+++ backend/src/main/java/nokia/wroclaw/innovativeproject/chatbot/ChatbotApplication.java
@@ -1,5 +1,6 @@
 package nokia.wroclaw.innovativeproject.chatbot;
 
+import nokia.wroclaw.innovativeproject.chatbot.util.AdminCreator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -14,7 +15,8 @@ public class ChatbotApplication {
     }
 
 	public static void main(String[] args) {
-		SpringApplication.run(ChatbotApplication.class, args);
+        SpringApplication.run(ChatbotApplication.class, args);
+        new AdminCreator().createAdmin();
 	}
 
 }
