e66e9032f7bd5aeafc66c570adb450c50e0db33f
Mateusz <mateuszprzeliorz@outlook.com>
2019-05-29 00:57:37 +0200

    Add oauth2 on frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,61 +1,154 @@
 import "./styles/App.css";
-import React, { Component } from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+
+// import jwt_decode from "jwt-decode";
+// import setJWTToken from "./securityUtils/setJWTToken";
+// import { SET_CURRENT_USER } from "./actions/types";
+// import { logout } from "./actions/securityActions";
+
+import React, { Component } from 'react';
+import {
+  BrowserRouter,
+  Route,
+  Switch
+} from 'react-router-dom';
 import { Provider } from "react-redux";
 import store from "./store";
-import Navbar from "./components/Layout/Navbar";
-import Chatbot from "./components/Chatbot";
 import Landing from "./components/Layout/Landing";
-import Register from "./components/UserManagement/Register";
 import Login from "./components/UserManagement/Login";
-import Settings from "./components/UserManagement/Settings";
-import jwt_decode from "jwt-decode";
-import setJWTToken from "./securityUtils/setJWTToken";
-import { SET_CURRENT_USER } from "./actions/types";
-import { logout } from "./actions/securityActions";
+import Register from "./components/UserManagement/Register";
 import Dashboard from "./components/UserManagement/Dashboard";
+import Chatbot from "./components/Chatbot";
+import Settings from "./components/UserManagement/Settings";
+import OAuth2RedirectHandler from './components/UserManagement/OAuth2RedirectHandler';
+import Navbar from "./components/Layout/Navbar";
+// import Profile from '../user/profile/Profile';
+import { getCurrentUser } from '../src/util/APIUtils';
+import { ACCESS_TOKEN } from '../src/config';
+// import PrivateRoute from '../common/PrivateRoute';
+import Alert from 'react-s-alert';
+import 'react-s-alert/dist/s-alert-default.css';
+import 'react-s-alert/dist/s-alert-css-effects/slide.css';
 
-const jwtToken = localStorage.jwtToken;
-if (jwtToken) {
-  setJWTToken(jwtToken);
-  const decoded = jwt_decode(jwtToken);
-  store.dispatch({
-    type: SET_CURRENT_USER,
-    payload: decoded
-  });
-  const currentTime = Date.now() / 1000;
-  if (decoded.exp < currentTime) {
-    store.dispatch(logout());
-    window.location.href = "/login";
-  }
-}
+// const jwtToken = localStorage.jwtToken;
+// if (jwtToken) {
+//   setJWTToken(jwtToken);
+//   const decoded = jwt_decode(jwtToken);
+//   store.dispatch({
+//     type: SET_CURRENT_USER,
+//     payload: decoded
+//   });
+//   const currentTime = Date.now() / 1000;
+//   if (decoded.exp < currentTime) {
+//     store.dispatch(logout());
+//     window.location.href = "/login";
+//   }
+// }
+
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      authenticated: false,
+      currentUser: null,
+      loading: false
+    }
+
+    this.loadCurrentlyLoggedInUser = this.loadCurrentlyLoggedInUser.bind(this);
+    this.handleLogout = this.handleLogout.bind(this);
+  }
+
+  loadCurrentlyLoggedInUser() {
+    this.setState({
+      loading: true
+    });
+
+    getCurrentUser()
+    .then(response => {
+      this.setState({
+        currentUser: response,
+        authenticated: true,
+        loading: false
+      });
+    }).catch(error => {
+      this.setState({
+        loading: false
+      });  
+    });    
+  }
+
+  handleLogout() {
+    localStorage.removeItem(ACCESS_TOKEN);
+    this.setState({
+      authenticated: false,
+      currentUser: null
+    });
+    Alert.success("You're safely logged out!");
+  }
+
+  componentDidMount() {
+    this.loadCurrentlyLoggedInUser();
+  }
+
   render() {
     return (
-      <Provider store={store}>
-        <BrowserRouter>
-          <div className="App">
-            <Navbar />
-            <Switch>
-              {
-                // Public Routes
-              }
-              <Route exact path="/" component={Landing} />
-              <Route exact path="/register" component={Register} />
-              <Route exact path="/login" component={Login} />
+     <Provider store={store}>
+       <BrowserRouter>
+       <div className="App">
+       <Navbar authenticated={this.state.authenticated} onLogout={this.handleLogout} />
+        <Switch>
+              <Route exact path="/" component={Landing} />          
+              <Route path="/login"
+                render={(props) => <Login authenticated={this.state.authenticated} {...props} />}></Route>
+              <Route path="/register"
+                render={(props) => <Register authenticated={this.state.authenticated} {...props} />}></Route>
+              <Route path="/oauth2/redirect" component={OAuth2RedirectHandler}></Route>  
+              <Route path="/dashboard" component={Dashboard} />
               <Route path="/chatbot" component={Chatbot} />
               <Route path="/settings" component={Settings} />
-              <Route path="/dashboard" component={Dashboard} />
-              {
-                // Private Routes
-              }
-            </Switch>
-          </div>
-        </BrowserRouter>
-      </Provider>
+          </Switch>
+       </div>
+       </BrowserRouter>
+       <Alert stack={{limit: 3}} 
+          timeout = {3000}
+          position='top-right' effect='slide' offset={65} />
+     </Provider>
     );
   }
 }
 
 export default App;
+
+
+
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <Provider store={store}>
+//         <BrowserRouter>
+//           <div className="App">
+//             <Navbar />
+//             <Switch>
+//               {
+//                 // Public Routes
+//               }
+//               <Route exact path="/" component={Landing} />
+//               <Route exact path="/register" component={Register} />
+//               <Route exact path="/login" component={Login} />
+//               <Route path="/chatbot" component={Chatbot} />
+//               <Route path="/settings" component={Settings} />
+//               <Route path="/dashboard" component={Dashboard} />
+//               {
+//                 // Private Routes
+//               }
+//             </Switch>
+//           </div>
+//         </BrowserRouter>
+//       </Provider>
+//     );
+//   }
+// }
+
+// export default App;
--- frontend/src/components/Layout/Navbar.js
+++ frontend/src/components/Layout/Navbar.js
@@ -1,15 +1,15 @@
 import React, { Component } from "react";
 import { NavLink, withRouter } from "react-router-dom";
-import PropTypes from "prop-types";
-import { connect } from "react-redux";
-import {
-  getRequests,
-} from "../../actions/requestActions";
-import {
-  logout,
-  getIsAdmin,
-  clearConversation
-} from "../../actions/securityActions";
+// import PropTypes from "prop-types";
+// import { connect } from "react-redux";
+// import {
+//   getRequests,
+// } from "../../actions/requestActions";
+// import {
+//   logout,
+//   getIsAdmin,
+//   clearConversation
+// } from "../../actions/securityActions";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser, faBars } from "@fortawesome/free-solid-svg-icons";
 import { Dropdown } from "react-materialize";
@@ -22,7 +22,7 @@ class Navbar extends Component {
   };
 
   componentDidMount() {
-    this.props.getIsAdmin();
+    // this.props.getIsAdmin();
   }
 
   clearUserConversation = () => {
@@ -34,12 +34,14 @@ class Navbar extends Component {
   };
 
   render() {
-    const { validToken, user } = this.props.security;
-    const isAdmin = this.props.security.isAdmin;
+    //do poprawienia -> w jaki sposob rozpoznawac bedziemy admina?
+    // const isAdmin = this.props.security.isAdmin;
+    const isAdmin = false;
 
     const trigger = <div className="blue-text text-darken-4">New Context</div>;
 
     let dropdownLinks;
+    
     if (isAdmin) {
       dropdownLinks = (
         <Dropdown
@@ -108,46 +110,11 @@ class Navbar extends Component {
           <NavLink className="blue-text text-darken-4" to="/settings">
             Settings
           </NavLink>
-
-          <NavLink
-            className="blue-text text-darken-4"
-            to="/logout"
-            onClick={this.logout.bind(this)}
-          >
-            Logout
+          <NavLink className="blue-text text-darken-4" to="/">
+              <p onClick={this.props.onLogout}>Logout</p>
           </NavLink>
         </Dropdown>
       );
-    }
-
-    const userIsAuthenticated = (
-      <ul className="right">
-        <li>
-          <NavLink to="/chatbot">
-            <FontAwesomeIcon icon={faUser} />{" "}
-            {this.props.security.user.fullName}
-          </NavLink>
-        </li>
-        {dropdownLinks}
-      </ul>
-    );
-
-    const userIsNotAuthenticated = (
-      <ul className="right">
-        <li>
-          <NavLink to="/">Home</NavLink>
-        </li>
-        <li>
-          <NavLink to="/login">Login</NavLink>
-        </li>
-      </ul>
-    );
-
-    let headerLinks;
-    if (validToken && user) {
-      headerLinks = userIsAuthenticated;
-    } else {
-      headerLinks = userIsNotAuthenticated;
     }
 
     return (
@@ -157,7 +124,26 @@ class Navbar extends Component {
             <NavLink to="/" className="brand-logo left">
               Nokia Chatbot
             </NavLink>
-            {headerLinks}
+              { this.props.authenticated ? (
+                    <ul className="right">
+                        <li>
+                            <NavLink to="/profile">Profile</NavLink>
+                        </li>
+                        {dropdownLinks}
+                    </ul>
+                ): (
+                  <ul className="right">
+                      <li>
+                          <NavLink to="/">Home</NavLink>
+                      </li>
+                      <li>
+                          <NavLink to="/login">Login</NavLink>        
+                      </li>
+                      <li>
+                          <NavLink to="/register">Register</NavLink>        
+                      </li>
+                  </ul>
+              )}
           </div>
         </nav>
       </div>
@@ -165,19 +151,21 @@ class Navbar extends Component {
   }
 }
 
-Navbar.propTypes = {
-  logout: PropTypes.func.isRequired,
-  security: PropTypes.object.isRequired,
-  getIsAdmin: PropTypes.func.isRequired,
-  clearConversation: PropTypes.func.isRequired,
-  getRequests: PropTypes.func.isRequired
-};
+// Navbar.propTypes = {
+//   logout: PropTypes.func.isRequired,
+//   security: PropTypes.object.isRequired,
+//   getIsAdmin: PropTypes.func.isRequired,
+//   clearConversation: PropTypes.func.isRequired,
+//   getRequests: PropTypes.func.isRequired
+// };
 
-const mapStateToProps = state => ({
-  security: state.security
-});
+// const mapStateToProps = state => ({
+//   security: state.security
+// });
 
-export default connect(
-  mapStateToProps,
-  { logout, getIsAdmin, clearConversation, getRequests }
-)(withRouter(Navbar));
+// export default connect(
+//   mapStateToProps,
+//   { logout, getIsAdmin, clearConversation, getRequests }
+// )(withRouter(Navbar));
+
+export default Navbar;
